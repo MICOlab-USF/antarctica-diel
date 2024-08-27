fa095a4a1c15ff107536840223912e2080cf04f7
Nilron1234 <anorlin@usf.edu>
2024-08-26 22:49:13 -0400

    optimized code using apply instead of iterating through a for loop

--- Row_analysis.R
+++ Row_analysis.R
@@ -7,6 +7,59 @@ create_plot = function(x_vec,y_vec,lm_vec){
 Input_data <- Eukaryota.merged[1:1000,]
 # Input_data <- read.csv("CSV_files/Bacteria.subsection.TPM.csv")
 
+vec_assembly <- unique(meta.data$AssemblyGroup)
+
+for(i in 1:length(vec_assembly)){
+  if(i == 1){
+    names(Input_data) <- unlist(lapply(names(Input_data),
+                                       function(x){unlist(strsplit(x,
+                                                                   split = "_",
+                                                                   fixed = TRUE))[1]}))
+    
+    df_meta <- meta.data[,c("SampleID","par","AssemblyGroup")]
+    
+    df_means <- Input_data$SequenceID
+    par_vec <- NA
+    time1 <- Sys.time()
+  }
+  
+  df_temp <- Input_data[,meta.data$SampleID[meta.data$AssemblyGroup == vec_assembly[i]]]
+  
+  meta_temp <- df_meta %>% 
+    distinct(AssemblyGroup,.keep_all = TRUE) %>% 
+    filter(AssemblyGroup == vec_assembly[i])
+  
+  par_vec[i] <- meta_temp$par
+  
+  vec <- rowMeans(df_temp)
+  df_means <- cbind.data.frame(df_means,vec)
+  
+  names(df_means)[i+1] <- vec_assembly[i]
+  
+  if(i == length(vec_assembly)){
+    # df_means <- as.data.frame(t(df_means))
+    names(df_means)[1] <- "SequenceID"
+    
+    df_means$r.squared <- apply(df_means[,2:9],1,function(row){summary(lm(row~par_vec))$r.squared})
+    df_means$n_nonzero <- apply(df_means[,2:9],1,function(row){sum(row > 0)})
+    
+    seq_df <- df_means[df_means$r.squared >.95 & df_means$n_nonzero > 2,]
+    
+    print(Sys.time() - time1)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+# rowMeans(Input_data[,meta.data$SampleID[meta.data$AssemblyGroup == vec_assembly[1]]])
+
 for(i in 1:nrow(Input_data)){
   if(i == 1){
     time1 <- Sys.time()
@@ -61,7 +114,7 @@ for(i in 1:nrow(Input_data)){
 
 
 
-seq_df <- read.csv("CSV_files/Eukaryota_row_reg.csv")
+seq_df2 <- read.csv("CSV_files/Eukaryota_row_reg.csv")
 filter <- Eukaryota.merged$SequenceID %in% names(seq_df)
 Eukaryota_light <- Eukaryota.merged[filter,]
 dfOut <- Eukaryota_light
@@ -95,3 +148,4 @@ ggplot(Eukaryota.merged, aes(x = Class))+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 # ggsave("All_Class.png",dpi = 300,height = 5, width = 6)
+
