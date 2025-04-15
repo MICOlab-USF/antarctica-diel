189f3085c17d60dbfb986acff72ce2d154f10dc8
Nilron1234 <anorlin@usf.edu>
2025-04-15 15:30:45 -0400

    Updated KEGG API, to be able to extract information about the module a ko is part of and the definition of that module

--- pathway_API.R
+++ pathway_API.R
@@ -3,6 +3,8 @@ pathway_API <- function(input,ReturnFull = 0){
   
   Name_idx <- grep("NAME",pathway)
   Class_idx <- grep("CLASS",pathway)
+  Module_idx <- grep("^MODULE|^\\s+M\\d{5}", pathway)
+  Definition_idx <- grep("DEFINITION",pathway)
   
   RelPaths_idx <- grep("REL_PATHWAY",pathway)
   RelPaths_end <- grep("///",pathway)-1
@@ -15,6 +17,19 @@ pathway_API <- function(input,ReturnFull = 0){
                   NA,
                   sub("^\\S+\\s+", "", pathway[Class_idx]))
   
+  if(length(Module_idx) == 0){
+    module <- NA
+  }else{
+    module <- unlist(regmatches(pathway[Module_idx],gregexpr("\\bM\\d{5}\\b", pathway[Module_idx])))
+  }
+  
+  if(isEmpty(Definition_idx)){
+    definition <- NA
+  }else{
+    definition <- unique(unlist(regmatches(pathway[Definition_idx],gregexpr("K\\d{5}\\b", pathway[Definition_idx]))))
+  }
+  
+  
   if(!isEmpty(RelPaths_idx)){
     RelPaths <- sub(".*\\sko", "ko", pathway[RelPaths_idx:RelPaths_end])
   } else {
@@ -24,7 +39,9 @@ pathway_API <- function(input,ReturnFull = 0){
   output <- list("ko" = input,
                  "name" = name,
                  "class" = class,
-                 "Rel_paths" = RelPaths)
+                 "Rel_paths" = RelPaths,
+                 "module" = module,
+                 "definition" = definition)
   
   if(ReturnFull == 1){
     output$Full <- pathway
